be2db6eb896bffd630f83e933d90ebcf6b519402
Gerar <g3rard095@hotmail.com>
2022-06-05 19:38:01 -0500

    compute targets

--- DS_associate_2_azure.py
+++ DS_associate_2_azure.py
@@ -5,6 +5,7 @@ ws = Workspace.get(name='aml-workspace',
                    resource_group='aml-resources')
 '''
 ws = Workspace.from_config()
+
 def targets():
     '''
     what configuration have the azure ml
@@ -115,5 +116,77 @@ def getDataset():
     ds2 = Dataset.get_by_name(ws, 'img_files')
     #img_ds = Dataset.get_by_name(workspace=ws, name='img_files', version=2)
 
+def createEnviroment():
+    from azureml.core import Environment
+    env = Environment.from_conda_specification(name='training_environment',  file_path='./conda.yml')
 
-print(createDataset())
+def createComputeTarget():
+    from azureml.core import Workspace
+    from azureml.core.compute import ComputeTarget, AmlCompute
+
+    # Load the workspace from the saved config file
+    ws = Workspace.from_config()
+
+    # Specify a name for the compute (unique within the workspace)
+    compute_name = 'aml-cluster'
+
+    # Define compute configuration
+    compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_DS11_V2',
+                                                           min_nodes=0, max_nodes=4,
+                                                           vm_priority='dedicated')
+
+    # Create the compute
+    aml_cluster = ComputeTarget.create(ws, compute_name, compute_config)
+    aml_cluster.wait_for_completion(show_output=True)
+
+def attachComputeTarget():
+    from azureml.core import Workspace
+    from azureml.core.compute import ComputeTarget, DatabricksCompute
+
+    # Load the workspace from the saved config file
+    ws = Workspace.from_config()
+
+    # Specify a name for the compute (unique within the workspace)
+    compute_name = 'db_cluster'
+
+    # Define configuration for existing Azure Databricks cluster
+    db_workspace_name = 'db_workspace'
+    db_resource_group = 'db_resource_group'
+    db_access_token = '1234-abc-5678-defg-90...'
+    db_config = DatabricksCompute.attach_configuration(resource_group=db_resource_group,
+                                                       workspace_name=db_workspace_name,
+                                                       access_token=db_access_token)
+
+    # Create the compute
+    databricks_compute = ComputeTarget.attach(ws, compute_name, db_config)
+    databricks_compute.wait_for_completion(True)
+
+def checkComputeTarget():
+    from azureml.core.compute import ComputeTarget, AmlCompute
+    from azureml.core.compute_target import ComputeTargetException
+
+    compute_name = "aml-cluster"
+
+    # Check if the compute target exists
+    try:
+        aml_cluster = ComputeTarget(workspace=ws, name=compute_name)
+        print('Found existing cluster.')
+    except ComputeTargetException:
+        # If not, create it
+        compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_DS11_V2',
+                                                               max_nodes=4)
+        aml_cluster = ComputeTarget.create(ws, compute_name, compute_config)
+
+    aml_cluster.wait_for_completion(show_output=True)
+
+def SelectComputeTarget():
+    from azureml.core import Environment, ScriptRunConfig
+
+    compute_name = 'aml-cluster'
+
+    training_env = Environment.get(workspace=ws, name='training_environment')
+
+    script_config = ScriptRunConfig(source_directory='my_dir',
+                                    script='script.py',
+                                    environment=training_env,
+                                    compute_target=compute_name)
