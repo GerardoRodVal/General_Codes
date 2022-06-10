7c653e6f295a3701d52feef12c64a76005f32263
Gerar <g3rard095@hotmail.com>
2022-06-10 11:01:06 -0500

    last update

--- DS_associate_2_azure.py
+++ DS_associate_2_azure.py
@@ -228,3 +228,102 @@ def early_termination():
     early_termination_policy = TruncationSelectionPolicy(truncation_percentage=10,
                                                          evaluation_interval=1,
                                                          delay_evaluation=5)
+
+def differential_privacy():
+    '''
+    Agregar ruido a los datos para incluir aleatoridad al analisis, no determinista
+
+    A lower epsilon reduces the impact of an individual's data on aggregated results, increasing privacy and reducing accuracy
+    '''
+
+def feature_importance():
+    '''
+    Model explainers use statistical techniques to calculate feature importance.
+     This enables you to quantify the relative influence each feature in the training dataset has on label prediction.
+    Explainers work by evaluating a test data set of feature cases and the labels the model predicts for them.
+    MimicExplainer - An explainer that creates a global surrogate model that approximates your trained model and can be used to generate explanations. This explainable model must have the same kind of architecture as your trained model (for example, linear or tree-based).
+    TabularExplainer - An explainer that acts as a wrapper around various SHAP explainer algorithms, automatically choosing the one that is most appropriate for your model ar74   chitecture.
+    PFIExplainer - a Permutation Feature Importance explainer that analyzes feature importance by shuffling feature values and measuring the impact on prediction performance.
+    '''
+    # MimicExplainer
+    from interpret.ext.blackbox import MimicExplainer
+    from interpret.ext.glassbox import DecisionTreeExplainableModel
+
+    mim_explainer = MimicExplainer(model=loan_model,
+                                   initialization_examples=X_test,
+                                   explainable_model=DecisionTreeExplainableModel,
+                                   features=['loan_amount', 'income', 'age', 'marital_status'],
+                                   classes=['reject', 'approve'])
+
+    # TabularExplainer
+    from interpret.ext.blackbox import TabularExplainer
+
+    tab_explainer = TabularExplainer(model=loan_model,
+                                     initialization_examples=X_test,
+                                     features=['loan_amount', 'income', 'age', 'marital_status'],
+                                     classes=['reject', 'approve'])
+
+    # PFIExplainer
+    from interpret.ext.blackbox import PFIExplainer
+
+    pfi_explainer = PFIExplainer(model=loan_model,
+                                 features=['loan_amount', 'income', 'age', 'marital_status'],
+                                 classes=['reject', 'approve'])
+
+    #Explaining global feature importance
+
+    # MimicExplainer
+    global_mim_explanation = mim_explainer.explain_global(X_train)
+    global_mim_feature_importance = global_mim_explanation.get_feature_importance_dict()
+
+    # TabularExplainer
+    global_tab_explanation = tab_explainer.explain_global(X_train)
+    global_tab_feature_importance = global_tab_explanation.get_feature_importance_dict()
+
+    # PFIExplainer
+    global_pfi_explanation = pfi_explainer.explain_global(X_train, y_train)
+    global_pfi_feature_importance = global_pfi_explanation.get_feature_importance_dict()
+
+    # Explaining local feature importance
+
+    # MimicExplainer
+    local_mim_explanation = mim_explainer.explain_local(X_test[0:5])
+    local_mim_features = local_mim_explanation.get_ranked_local_names()
+    local_mim_importance = local_mim_explanation.get_ranked_local_values()
+
+    # TabularExplainer
+    local_tab_explanation = tab_explainer.explain_local(X_test[0:5])
+    local_tab_features = local_tab_explanation.get_ranked_local_names()
+    local_tab_importance = local_tab_explanation.get_ranked_local_values()
+
+def createExplainer():
+    # Import Azure ML run library
+    from azureml.core.run import Run
+    from azureml.contrib.interpret.explanation.explanation_client import ExplanationClient
+    from interpret.ext.blackbox import TabularExplainer
+    # other imports as required
+
+    # Get the experiment run context
+    run = Run.get_context()
+
+    # code to train model goes here
+
+    # Get explanation
+    explainer = TabularExplainer(model, X_train, features=features, classes=labels)
+    explanation = explainer.explain_global(X_test)
+
+    # Get an Explanation Client and upload the explanation
+    explain_client = ExplanationClient.from_run(run)
+    explain_client.upload_model_explanation(explanation, comment='Tabular Explanation')
+
+    # Complete the run
+    run.complete()
+
+    # viweing the explanation
+
+    from azureml.contrib.interpret.explanation.explanation_client import ExplanationClient
+
+    client = ExplanationClient.from_run_id(workspace=ws,
+                                           experiment_name=experiment.experiment_name,
+                                           run_id=run.id)
+    explanation = client.download_model_explanation()
